96f2a4543ac62fc77d81a223587b3d54a893fc91
Luke Flannery <lukeflannery@Lukes-MBP-2.broadband>
2020-05-05 14:45:35 +0100

    comment card recfactored to functional comp after loss of use for state

--- flannery-nc-news-react/src/components/comment.card.js
+++ flannery-nc-news-react/src/components/comment.card.js
@@ -1,33 +1,30 @@
 import React from "react";
 import CommentVoteUpdater from "./Comment.voteUpdater";
 
-class CommentCard extends React.Component {
-  render() {
-    const { comment, deleteComment } = this.props;
-    const humanDate = new Date(comment.created_at).toDateString();
+function CommentCard(props) {
+  const { comment, deleteComment } = props;
+  const humanDate = new Date(comment.created_at).toDateString();
 
-    return (
-      <li className="comment">
-        <p>Author: {comment.author}</p>
-        <p>ID: {comment.article_id}</p>
-        <p>Comment ID: {comment.comment_id}</p>
-        <p>Votes: {comment.votes}</p>
-        <p>Body: {comment.body}</p>
-        <p>Created at: {humanDate}</p>
-        <CommentVoteUpdater
-          votes={comment.votes}
-          id={comment.article_id}
-          comment_id={comment.comment_id}
-        />
+  return (
+    <li className="comment">
+      <p>Author: {comment.author}</p>
+      <p>ID: {comment.article_id}</p>
+      <p>Comment ID: {comment.comment_id}</p>
+      <p>Created at: {humanDate}</p>
+      <p>Body: {comment.body}</p>
+      <CommentVoteUpdater
+        votes={comment.votes}
+        id={comment.article_id}
+        comment_id={comment.comment_id}
+      />
 
-        {comment.author === "jessjelly" ? (
-          <section>
-            <button onClick={deleteComment}>delete comment</button>
-          </section>
-        ) : null}
-      </li>
-    );
-  }
+      {comment.author === "jessjelly" ? (
+        <section>
+          <button onClick={deleteComment}>delete comment</button>
+        </section>
+      ) : null}
+    </li>
+  );
 }
 
 export default CommentCard;
